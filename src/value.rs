d10b8d7a10f66575e27f7580562607f818847ba3
Jacob Birkett <spikespaz@outlook.com>
2023-09-25 17:47:55 -0700

    value: impl From for Value

--- src/value.rs
+++ src/value.rs
@@ -16,3 +16,33 @@ pub enum Value {
     List(Vec<Value>),
     Null,
 }
+
+macro_rules! impl_from {
+    ($from:ty => $variant:ident) => {
+        impl From<$from> for Value {
+            fn from(other: $from) -> Self {
+                Self::$variant(other)
+            }
+        }
+
+        impl From<Option<$from>> for Value {
+            fn from(other: Option<$from>) -> Self {
+                other.map_or(Self::Null, Into::into)
+            }
+        }
+    };
+}
+
+impl_from!(u8 => U8);
+impl_from!(u16 => U16);
+impl_from!(u32 => U32);
+impl_from!(u64 => U64);
+impl_from!(usize => Uint);
+impl_from!(i8 => I8);
+impl_from!(i16 => I16);
+impl_from!(i32 => I32);
+impl_from!(i64 => I64);
+impl_from!(isize => Int);
+impl_from!(bool => Bool);
+impl_from!(String => String);
+impl_from!(Vec<Value> => List);
